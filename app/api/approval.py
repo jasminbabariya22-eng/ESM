0cbc066bd199947c4e1f65fa387c67e74e960433
jasminbabariya22-eng <jasminbabariya22@gmail.com>
2026-03-17 17:23:42 +0530

    update Approved api

--- app/api/approval.py
+++ app/api/approval.py
@@ -8,6 +8,7 @@ from app.schemas.risk_approval import RiskApprovalRequest
 from app.services.risk_approval import approve_risk
 
 from datetime import date
+from app.models.user import User
 
 router = APIRouter(prefix="/approval", tags=["Approval"], dependencies=[Depends(get_current_user)])
 
@@ -39,13 +40,21 @@ def approve_risk_api(
     try:
         risk, status_name = approve_risk(db, data, current_user["id"])
 
+        # ✅ Fetch user login_id
+        user_obj = db.query(User).filter(
+            User.id == current_user["id"]
+        ).first()
+
+        approval_by_name = user_obj.log_id if user_obj else None
+
         return success_response(
             data={
                 "risk_register_id": data.risk_register_id,
                 "approval_level": data.approval_level,
                 "approval_status_id": data.approval_status_id,
-                "risk_status_name": status_name,  
-                "risk_approval_on":date.today(),
+                "risk_status_name": status_name,
+                "approval_by_name": approval_by_name,   # ✅ added
+                "risk_approval_on": date.today(),
                 "remark": data.remark
             }
         )
